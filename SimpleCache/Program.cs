64eea1e97304523043c22dc91b4e3066e0e2fecb
ElayQ <vadimgazizovq@gmail.com>
2023-07-27 08:23:43 +0500

    Simple cache

---
+++ SimpleCache/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+SimpleCache c = new();
+c.factorial(5);
+c.factorial(5);
+c.factorial(5);
+c.factorial(3);
+c.factorial(4);
+c.factorial(3);
+c.factorial(5);
+c.factorial(3);
+c.factorial(4);
---
+++ SimpleCache/SimpleCache.cs
@@ -0,0 +1,22 @@
+﻿public class SimpleCache
+{
+    private Dictionary<int, int> _cache = new();
+    private void GetOrCreate(int key, Func<int, int> createItem)
+    {
+        if (_cache.ContainsKey(key))
+        {
+            Console.WriteLine("Взято из кеша: {0}", _cache[key]);
+            return;
+        }
+        int result = createItem(key);
+        _cache[key] = result;
+        Console.WriteLine("Взято из функции: {0}", _cache[key]);
+    }
+    private int Factorial(int n)
+    {
+        if (n == 1) return n;
+        return n * Factorial(n - 1);
+    }
+
+    public void factorial(int n) => GetOrCreate(n, Factorial);
+}
